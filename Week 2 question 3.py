d13052bb22f073191ad27b1492bb533817ca6907
GKE20 <evansg16@uni.coventry.ac.uk>
2016-12-05 19:54:08 +0000

    Add files via upload
     I was told by David in my Viva to upload this as I had only uploaded the pseudocode for this question, but I also had the actual code, which I showed in the Viva.

---
+++ Week 2 question 3.py
@@ -0,0 +1,19 @@
+from math import *
+def HPS (N):
+    A = sqrt(N) #Square root of N (input number)
+    B = int(A) #Turning it to an integer becuase it could be a float because
+               #it won't always be a whole number and could be a decimal
+    C = B**2 #Performs B to the power of 2 to get the closest whole number
+             #and square number
+    print ((str('')) + (str(C)))
+HPS(20)    
+
+
+
+
+
+
+
+
+
+
